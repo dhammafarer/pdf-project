42160f7f125c12803fc7eb126fad7f86f47ecb31
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-03-20 17:13:33 +0800

    change margins

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ export default {
   ...colors,
   pageOrientation: 'portrait',
   pageSize: 'A4',
-  pageMargins: [40,40,40,60],
+  pageMargins: [60, 40],
   noMargin: [-42, 0],
   fw: 600,
   cols: 8,
